ff07b4ec7e4c906400d1ef62c4db32f46c68231c
Xihan Zhang <zhang_xihan@hotmail.com>
2018-11-28 10:41:24 -0600

    Project Initialization

---
+++ assignment7/ClientMain.java
@@ -0,0 +1,7 @@
+package assignment7;
+
+public class ClientMain {
+    public static void main(String[] args){
+
+    }
+}
---
+++ assignment7/ServerMain.java
@@ -0,0 +1,7 @@
+package assignment7;
+
+public class ServerMain {
+    public static void main (String[]args){
+        System.out.println("start");
+    }
+}
