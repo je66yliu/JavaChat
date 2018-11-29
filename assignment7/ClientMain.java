90fc3f22fe437fa75758e4a9ac7c63ff95280fdd
Xihan Zhang <zhang_xihan@hotmail.com>
2018-11-29 15:46:53 -0600

    Client-Server connected; Basic UI

--- assignment7/ClientMain.java
+++ assignment7/ClientMain.java
@@ -1,7 +1,121 @@
 package assignment7;
 
-public class ClientMain {
-    public static void main(String[] args){
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+import java.beans.EventHandler;
+import java.io.*;
+import java.net.Socket;
+import java.util.Collection;
+
+public class ClientMain extends Application {
+    private int WINDOW_WIDTH;
+    private int WINDOW_HEIGHT;
+    private TextArea incoming;
+    private TextField outgoing;
+    private BufferedReader reader;
+    private PrintWriter writer;
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private void connetToServer() throws IOException {
+        @SuppressWarnings("resource")
+        Socket clientSock = new Socket("127.0.0.1", 5000);
+        InputStreamReader streamReader = new InputStreamReader(clientSock.getInputStream());
+        reader = new BufferedReader(streamReader);
+        writer = new PrintWriter(clientSock.getOutputStream());
+        System.out.println("Connected to the server");
+        Thread readerThread = new Thread(new IncomingReader());
+        readerThread.start();
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        connetToServer();
+
+        primaryStage.setTitle("Pair-40 Chat Room");
+
+        /***** Set up text output *****/
+        Label label_ChatHistory = new Label("Chat History");
+        incoming = new TextArea();
+        incoming.setMaxHeight(200);
+        incoming.setMaxWidth(400);
+        incoming.setEditable(false);
+
+        //Auto scroll to bottom when received new messages
+        /*
+        incoming.textProperty().addListener(new ChangeListener<Object>() {
+            @Override
+            public void changed(ObservableValue<?> observable, Object oldValue, Object newValue) {
+                incoming.setScrollTop(Double.MAX_VALUE);
+            }
+        });*/
+
+
+        /***** Set up text input *****/
+        Label label_EnterText = new Label("Enter Text Here");
+        outgoing = new TextField();
+        outgoing.setMaxWidth(400);
+        outgoing.setMaxHeight(20);
+        Button sendText = new Button("Send");
+
+
+        sendText.setOnAction(e->{
+            System.out.println(outgoing.getText());
+            writer.println(outgoing.getText());
+            writer.flush();
+            outgoing.setText("");
+            outgoing.requestFocus();
+        });
+
+        HBox textInput = new HBox();
+        textInput.getChildren().addAll(outgoing, sendText);
+
+
+        /***** Main Control *****/
+        VBox mainBox = new VBox();
+        mainBox.getChildren().addAll(label_ChatHistory, incoming, label_EnterText, textInput);
+
+        GridPane mainGrid = new GridPane();
+        mainGrid.getChildren().addAll(mainBox);
+
+        Scene mainScene = new Scene(mainGrid, 500, 500);
+
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
+    }
+
+
+    class IncomingReader implements Runnable {
+
+        @Override
+        public void run() {
+            String message;
+
+            try {
+                while ((message = reader.readLine()) != null) {
+                    incoming.appendText(message + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- assignment7/ServerMain.java
+++ assignment7/ServerMain.java
@@ -1,7 +1,66 @@
 package assignment7;
 
-public class ServerMain {
-    public static void main (String[]args){
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Observable;
+
+public class ServerMain extends Observable {
+
+    int port =5000;
+
+    public static void main(String[] args) {
         System.out.println("start");
+        try{
+            new ServerMain().initNetwork();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private void initNetwork() throws Exception {
+        @SuppressWarnings("resource")
+        ServerSocket serverSocket = new ServerSocket(port);
+
+        while(true){
+            Socket clientSock = serverSocket.accept();
+            ClientObserver writer = new ClientObserver(clientSock.getOutputStream());
+            Thread t = new Thread(new ClientHandler(clientSock));
+            t.start();
+            this.addObserver(writer);
+            System.out.println("Got a connection from " + clientSock);
+        }
+    }
+
+    class ClientHandler implements Runnable {
+        private BufferedReader reader;
+
+        public ClientHandler(Socket clientSock) {
+            Socket sock = clientSock;
+            try {
+                reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        @Override
+        public void run() {
+            String message;
+            try {
+                while ((message = reader.readLine()) != null) {
+                    System.out.println("Sever read: " + message);
+                    setChanged();
+                    notifyObservers(message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
